19995b98a067aa14f693c01927aae5b51625a662
Sveta Demianova <svetademianova@Svetas-MacBook-Air.local>
2024-03-02 09:59:19 +0100

    TfidfVectorizer's added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #importing libraries
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np, pandas as pd, matplotlib as mlt
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import os
 
 #importing database
-dataset = pd.read_csv("data_set_4.csv")
+dataset = pd.read_csv("fake-news/data_set_4.csv")
 print(dataset.size)
 
 '''
@@ -49,7 +50,7 @@ print('x train=',len(x_train),
       "\t",'y test=',len(y_test))
 
 
-file_path = 'ukrainian'
+file_path = 'fake-news/ukrainian'
 # Check if the file exists
 if os.path.isfile(file_path):
     with open(file_path, "r") as f:
@@ -61,5 +62,31 @@ else:
 stopwords = raw_ukr.split("\n")
 print(stopwords)
 
-# TfidfVectorizer
+# TfidfVectorizer - Term Frequency Inverse Document Frequency - an overall document weightage:
+#                   TF-IDF(t) = TF(t,d) * IDF(t)
+# CountVectorizer - gives number of frequency with respect to index of vocabulary.
+# TfidfTransformer
+'''
+ContVectorizer() gives number of frequency with respect to index of vocabulary. 
+Tf-idf conciders overall documents of weight of words.
+
+TF-IDF = TF(t,d) * IDF(t),      where TF(t,d) is a number of times term 't' appears in a doc 'd'; 
+                                and IDF(t) = log((1+n) / (1+df(d,t)) +1), where n = amount of documents, 
+                                df(d,t) - document frequency of the term t
+
+                                Term-Frequency Func:        TF(t,d) = sum(fr(x,t)) in range x є d   
+                                                            where fr(x,t)={[1, if x=t] or [0, otherwise]
+                                
+                                                            TF(t,d) returns how many times the term 't' is present in
+                                                            document 'd' 
+
+        The TF-IDF weight:
+                The idf(inverse document frequency) is defined: IDF(t) = log(|D| / 1+|{d : t є d}|),
+                where |{d : t є d}| is the number of documents where the term 't' appears, when TF(t,d) != 0
+'''
+
+tfidfVectorizer = TfidfVectorizer(stop_words=stopwords,max_df=0.7,strip_accents=None)
+tfidf_train=tfidfVectorizer.fit_transform(x_train.values.astype('U')) 
+tfidf_test=tfidfVectorizer.transform(x_test.values.astype('U'))
+
 # PassiveAggressiveClassifier
